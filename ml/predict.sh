ee5f553d05b9595ba0159998479d09e0ee827c90
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-04-12 18:04:30 -0700

    Only store the global origin of the bone.

--- ml/predict.sh
+++ ml/predict.sh
@@ -1,1 +1,4 @@
- ludwig predict --dataset test.tsv --model_path ./results/experiment_run_101/model --skip_save_unprocessed_output  --output_directory output
+ ludwig predict --dataset test.tsv --model_path ./results/experiment_run_51/model --skip_save_unprocessed_output  --output_directory output --skip_save_unprocessed_output
+# pip install -e ".[serve]"
+ ludwig serve --model_path ./results/experiment_run_51/model
+curl http://0.0.0.0:8000/predict -X POST -d "@data.txt"
