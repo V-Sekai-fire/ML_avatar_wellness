4f8c27747a55b68aed478564969567de151224a9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-09-18 12:33:27 -0700

    Update Node3D.gd, V-Sekai Adult Female - 2021 V-Sekai and 2020 Wonder Media.res, and 3 more files...

--- Node3D.gd
+++ Node3D.gd
@@ -1,10 +1,5 @@
 extends Node
 
-# Based on
-# https://github.com/JosephCatrambone/GodotSkeletonRemapper/blob/main/LICENSE
-# MIT License
-# Copyright (c) 2022 Joseph Catrambone
-
 func _ready():
 	var first : bool = true
 	var f = File.new()
@@ -22,27 +17,25 @@ func _ready():
 				var vrm_filename : String
 				vrm_extension = node
 				var human_map : BoneMap
+				var skeleton : Skeleton3D = null
 				if vrm_extension and vrm_extension.get("vrm_meta"):
 					human_map = vrm_extension["vrm_meta"]["humanoid_bone_mapping"]
 					vrm_meta = vrm_extension["vrm_meta"]
-#					vrm_filename = "res://%s - %s" % [vrm_meta.title, vrm_meta.author]
-#					ResourceSaver.save(human_map, vrm_filename + ".tres")
+					skeleton = vrm_extension.get_node_or_null(vrm_extension.vrm_skeleton)
 				if vrm_extension:
 					if first:
-						# DEBUG: Save a CSV of this data
 						var header : PackedStringArray
-						header.push_back("label")
-						header.push_back("source_bone")
+						for profile_bone_i in human_map.profile.bone_size:
+							header.push_back(human_map.profile.get_bone_name(profile_bone_i))
 						f.store_csv_line(header, "\t")
 						first = false
 					var line : PackedStringArray
-					var skeleton_line : PackedStringArray
-					for bone_i in range(0, human_map.profile.bone_size):
-						var profile_bone_name : String = human_map.profile.get_bone_name(bone_i)
-						var source_bone_name : String = human_map.get_skeleton_bone_name(profile_bone_name)
-						line.push_back(profile_bone_name)
-						line.push_back(source_bone_name)
-					line.push_back(", ".join(skeleton_line))
+					if skeleton != null:
+						var skeleton_line : PackedStringArray
+						for bone_i in human_map.profile.bone_size:
+							var source_bone_name : String = human_map.get_skeleton_bone_name(human_map.profile.get_bone_name(bone_i))
+							skeleton_line.push_back(source_bone_name)
+						line.push_back(" ".join(skeleton_line))
 					f.store_csv_line(line, "\t")
 			queue.append_array(node.get_children())
 			queue.pop_front()
