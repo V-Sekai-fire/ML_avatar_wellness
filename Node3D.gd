3bdf761e95baf5adef23f283c5bdabb0297dcbb7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-09-18 18:59:28 -0700

    Save work.

--- Node3D.gd
+++ Node3D.gd
@@ -25,17 +25,30 @@ func _ready():
 				if vrm_extension:
 					if first:
 						var header : PackedStringArray
-						for profile_bone_i in human_map.profile.bone_size:
-							header.push_back(human_map.profile.get_bone_name(profile_bone_i))
+						for bone_i in human_map.profile.bone_size:
+							var target_bone_name : String = human_map.profile.get_bone_name(bone_i)
+							header.push_back("profile_" + target_bone_name)
+						header.push_back("skeleton")
 						f.store_csv_line(header, "\t")
 						first = false
 					var line : PackedStringArray
 					if skeleton != null:
-						var skeleton_line : PackedStringArray
 						for bone_i in human_map.profile.bone_size:
-							var source_bone_name : String = human_map.get_skeleton_bone_name(human_map.profile.get_bone_name(bone_i))
-							skeleton_line.push_back(source_bone_name)
-						line.push_back(" ".join(skeleton_line))
-					f.store_csv_line(line, "\t")
+							var profile_bone_name : String = human_map.profile.get_bone_name(bone_i)
+							var target_bone_name : String = human_map.get_skeleton_bone_name(profile_bone_name)
+							if target_bone_name.is_empty():
+								line.push_back("PROFILE_BONE_UNKNOWN")
+							else:
+								line.push_back("%s" % [target_bone_name] )
+						var skeleton_line : PackedStringArray
+						for bone_i in skeleton.get_bone_count():
+							var bone_name = skeleton.get_bone_name(bone_i)
+							var target_bone_name : String = human_map.find_profile_bone_name(bone_name)
+							if target_bone_name.is_empty():
+								skeleton_line.push_back(bone_name)
+							else:
+								skeleton_line.push_back(target_bone_name)
+						line.push_back(", ".join(skeleton_line))
+						f.store_csv_line(line, "\t")
 			queue.append_array(node.get_children())
 			queue.pop_front()
--- ml/src/train.sh
+++ ml/src/train.sh
@@ -1,1 +1,3 @@
-ludwig train --dataset train.tsv  -c config.yml
+rm -rf results
+export NUMEXPR_MAX_THREADS=`nproc`
+ludwig train --dataset train.tsv -c config.yml
